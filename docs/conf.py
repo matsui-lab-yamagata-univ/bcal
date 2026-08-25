67749089f0f3e6960af346b6061140a819675d8f
subaru <subaru519kqka@gmail.com>
2026-08-25 22:10:39 +0900

    [update] update author list with new contributor Koki Ozawa

--- docs/conf.py
+++ docs/conf.py
@@ -9,8 +9,8 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[1] / "src"))
 
 # -- Project information -----------------------------------------------------
 project = "bcal"
-author = "Hiroyuki Matsui, Tomoharu Okada"
-copyright = "2026, Hiroyuki Matsui, Tomoharu Okada"
+author = "Tomoharu Okada, Koki Ozawa, Hiroyuki Matsui"
+copyright = "2026, Tomoharu Okada, Koki Ozawa, Hiroyuki Matsui"
 release = "0.1.0"
 version = "0.1.0"
 
